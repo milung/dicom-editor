29965af38e7c18e394aaf49a6e2e81394a496103
Lukas Beno <lukys137@gmail.com>
2017-07-18 16:18:02 +0200

    351 - Dicom table + test created

---
+++ src/client/components/dicom-table/dicom-table.tsx
@@ -0,0 +1,47 @@
+import * as React from 'react';
+import {
+    Table,
+    TableBody,
+    TableHeader,
+    TableHeaderColumn,
+    TableRow,
+    TableRowColumn,
+} from 'material-ui';
+
+import { DicomEntry } from '../../model/dicom-entry';
+
+interface DicomTableProps {
+    data: DicomEntry[];
+}
+
+interface DicomTableState {
+}
+
+export class DicomTable extends React.Component<DicomTableProps, DicomTableState> {
+    constructor(props: DicomTableProps) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <Table selectable={false}>
+                <TableHeader displaySelectAll={false} adjustForCheckbox={false}>
+                    <TableRow>
+                        <TableHeaderColumn>Tag Name</TableHeaderColumn>
+                        <TableHeaderColumn>Value</TableHeaderColumn>
+                    </TableRow>
+                </TableHeader>
+                <TableBody displayRowCheckbox={false}>
+                    {this.props.data.map(_ => {
+                        return (
+                            <TableRow>
+                                <TableRowColumn>{_.tagName}</TableRowColumn>
+                                <TableRowColumn>{_.tagValue}</TableRowColumn>
+                            </TableRow>
+                        );
+                    })}
+                </TableBody>
+            </Table>
+        );
+    }
+}
---
+++ test/client/dicom-table.test.tsx
@@ -0,0 +1,46 @@
+import * as React from 'react';
+import { DicomTable } from '../../src/client/components/dicom-table/dicom-table';
+import { shallow } from 'enzyme';
+import { expect } from 'chai';
+
+const arrayTest = [
+    {
+        tagGroup : '0152',
+        tagElement : '0145',
+        tagName: 'PatientName',
+        tagValue: 'Michal Mrkvicka'
+    },
+    {
+        tagGroup : '5478',
+        tagElement : '1548',
+        tagName: 'PatientAge',
+        tagValue: '18'
+    }
+];
+
+describe('dicom-table', () => {
+    it('Render DicomTable contains one table', () => {
+        const div = shallow(<DicomTable data={[]}/>);
+        expect(div.find('Table').length).to.equal(1);
+    });
+
+    it('Render DicomTable contains one table header', () => {
+        const div = shallow(<DicomTable data={[]}/>);
+        expect(div.find('TableHeader').length).to.equal(1);
+    });
+
+    it('Render DicomTable contains one table body', () => {
+        const div = shallow(<DicomTable data={[]}/>);
+        expect(div.find('TableBody').length).to.equal(1);
+    });
+
+    it('Render DicomTable with empty data creates empty table', () => {
+        const div = shallow(<DicomTable data={[]}/>);
+        expect(div.find('TableBody').find('TableRow').length).to.equal(0);
+    });
+
+    it('Render DicomTable with data creates table', () => {
+        const div = shallow(<DicomTable data={arrayTest}/>);
+        expect(div.find('TableBody').find('TableRow').length).to.equal(2);
+    });    
+});
