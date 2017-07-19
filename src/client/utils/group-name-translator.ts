e1e0be7030bcc738c29394fde4e4601a0a73a2d5
Filip Mrocek <filip.mrocek@unicornsystems.eu>
2017-07-19 15:51:50 +0200

    352 - refactoring + comments

--- src/client/utils/group-name-translator.ts
+++ src/client/utils/group-name-translator.ts
@@ -1,4 +1,8 @@
-
+/**
+ * Function tries to translate tag group number to its string name. If string name cannot
+ * be found, its original tag group name is returned
+ * @param tagGroup tag group name to be translated
+ */
 export function translateTagGroup(tagGroup: string): string {
     let translation = groupNameTranslation[tagGroup];
     if (translation === undefined) {
@@ -7,7 +11,12 @@ export function translateTagGroup(tagGroup: string): string {
     return translation;
 }
 
+/**
+ * List of all available translations for tag group numbers
+ */
 var groupNameTranslation = {
     '0008': 'EightName',
-    '0007': 'SevenName'
+    '0007': 'SevenName',
+    '0002': 'TwoName'
+    // this data needs to be filled
 }
