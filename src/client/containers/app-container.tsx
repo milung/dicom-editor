c1b9336ecf76ab28a87a882b1c75a69cde49958a
Filip Mrocek <filip.mrocek@unicornsystems.eu>
2017-07-20 23:50:26 +0200

    file names refactoring fix

---
+++ src/client/containers/app-container.tsx
@@ -0,0 +1,18 @@
+import * as React from 'react';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import { BrowserRouter, Route } from 'react-router-dom';
+import App from '../App';
+
+export default class AppContainer extends React.Component<{}, {}> {
+  render() {
+    return (
+      <div>
+        <MuiThemeProvider>
+          <BrowserRouter>
+            <Route path="/" component={App} />
+          </BrowserRouter>
+        </MuiThemeProvider>
+      </div>
+    );
+  }
+}
