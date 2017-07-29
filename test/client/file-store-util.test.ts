bcdd1e0908b79772bede66a1172dda7d3eba6f17
Filip Mrocek <mrocek.filip@gmail.com>
2017-07-29 21:37:25 +0200

    468 - added tests

---
+++ test/client/file-store-util.test.ts
@@ -0,0 +1,56 @@
+import { HeavyweightFile, LightweightFile } from './../../src/client/model/file-interfaces';
+import { expect } from 'chai';
+import { convertHeavyToLight } from '../../src/client/utils/file-store-util';
+describe('file-store convertHeavyToLight', () => {
+    it('should convert heavy file into light file', () => {
+        let heavy: HeavyweightFile = {
+            fileName: 'someName',
+            timestamp: 1,
+            fileSize: 1,
+            bufferedData: new Uint8Array(0),
+            dicomData: {
+                entries: [
+                    {
+                        tagGroup: 'gr',
+                        tagElement: '52',
+                        tagName: 'name',
+                        tagValue: 'val',
+                        tagVR: 'vr',
+                        tagVM: 'vm',
+                        colour: 'red',
+                    }
+                ]
+            }
+        }
+
+        let light: LightweightFile = {
+            dbKey: 'someName',
+            fileName: 'someName',
+            timestamp: 1,
+        }
+
+        expect(light).to.deep.equal(convertHeavyToLight(heavy));
+    });
+
+    it('should convert heavy file into light file', () => {
+        let heavy: HeavyweightFile = {
+            fileName: 'name',
+            timestamp: 123456789,
+            fileSize: 123,
+            bufferedData: new Uint8Array(8),
+            dicomData: {
+                entries: [
+
+                ]
+            }
+        }
+
+        let light: LightweightFile = {
+            dbKey: 'name',
+            fileName: 'name',
+            timestamp: 123456789,
+        }
+
+        expect(light).to.deep.equal(convertHeavyToLight(heavy));
+    });
+});
---
+++ test/client/side-bar.test.tsx
@@ -0,0 +1,35 @@
+import * as React from 'react';
+import { shallow } from 'enzyme';
+import { expect } from 'chai';
+import SideBar from '../../src/client/components/side-bar';
+import { ApplicationStateReducer } from '../../src/client/application-state';
+
+describe('dicom-table', () => {
+    it('should render side bar with 3 tabs', () => {
+        let reducer = new ApplicationStateReducer();
+        const sideBar = shallow(<SideBar reducer={reducer} />);
+        expect(sideBar.find('Paper').find('Tabs').find('Tab').length).to.equal(3);
+    });
+
+    it('should render side bar with first tab containing button', () => {
+        let reducer = new ApplicationStateReducer();
+        const sideBar = shallow(<SideBar reducer={reducer} />);
+        expect(sideBar.find('Paper').find('Tabs').childAt(0).find('RaisedButton').length).to.equal(1);
+    });
+
+    it('should render side bar with third tab containing button', () => {
+        let reducer = new ApplicationStateReducer();
+        const sideBar = shallow(<SideBar reducer={reducer} />);
+        expect(sideBar.find('Paper').find('Tabs').childAt(2).find('RaisedButton').length).to.equal(1);
+    });
+
+    it('should render all tabs with List component', () => {
+        let reducer = new ApplicationStateReducer();
+        const sideBar = shallow(<SideBar reducer={reducer} />);
+        let allHave = true;
+        sideBar.find('Paper').find('Tabs').children('Tab').map((el, index) => {
+            allHave = allHave && el.find('List').length === 1;
+        });
+        expect(allHave).to.equal(true);
+    });
+});
