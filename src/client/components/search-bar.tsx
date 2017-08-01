d352f6433bce967488aec4c996200f31a4766593
Lukas Beno <lukys137@gmail.com>
2017-08-01 11:17:51 +0200

    492 - delete button

--- src/client/components/search-bar.tsx
+++ src/client/components/search-bar.tsx
@@ -4,9 +4,11 @@ import { RaisedButton } from 'material-ui';
 import './search-bar.css';
 import TextField from 'material-ui/TextField';
 import { ApplicationStateReducer } from '../application-state';
+var ClearIcon = require('react-icons/lib/md/clear');
 
 interface SearchBarState {
     searchingValue: string;
+    defaultValue: string;
 }
 
 interface SearchBarProps {
@@ -17,7 +19,8 @@ export default class Search extends React.Component<SearchBarProps, SearchBarSta
     public constructor(props: SearchBarProps) {
         super(props);
         this.state = {
-            searchingValue: ''
+            searchingValue: '',
+            defaultValue: 'Text to find'
         };
     }
 
@@ -26,21 +29,33 @@ export default class Search extends React.Component<SearchBarProps, SearchBarSta
         this.props.reducer.setSearchExpression(value);
     }
 
+    removeSearching = () => {
+        this.props.reducer.setSearchExpression('');
+        this.setState({
+            searchingValue: '',
+            defaultValue: 'Text to find'
+        });
+    }
+
     render() {
         return (
             <div className="flex-container">
-                <TextField
-                    className="textField-override"
-                    hintText="Text to find"
-                    onKeyPress={(ev) => {
-                        if (ev.key === 'Enter') {
-                            { this.setSearching(); }
-                            ev.preventDefault();
-                        }
-                    }}
-                    onChange={(e: React.FormEvent<HTMLInputElement>) =>
-                        this.setState({ searchingValue: e.currentTarget.value })}
-                />
+                <div className="list-item">
+                    <TextField
+                        className="textField-override"
+                        hintText={this.state.defaultValue}
+                        onKeyPress={(ev) => {
+                            if (ev.key === 'Enter') {
+                                { this.setSearching(); }
+                                ev.preventDefault();
+                            }
+                        }}
+                        value={this.state.searchingValue}
+                        onChange={(e: React.FormEvent<HTMLInputElement>) =>
+                            this.setState({ searchingValue: e.currentTarget.value })}
+                    />
+                    <ClearIcon onClick={this.removeSearching} className="icon-override" />
+                </div>
                 <RaisedButton
                     primary={true}
                     label="Find"
