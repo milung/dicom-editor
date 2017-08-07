7c0a619a60a37b59a0bea48a64a14ae56a0bb63a
Lubomira Trnavska <lubomira.trnavska@gmail.com>
2017-08-07 16:12:59 +0200

    excessively long tag value is not displayed but downloaded

--- src/client/components/dicom-table/dicom-simple-table.tsx
+++ src/client/components/dicom-table/dicom-simple-table.tsx
@@ -31,44 +31,50 @@ export class DicomSimpleTable extends React.Component<DicomSimpleTableProps, Dic
                     <DicomTableHeader />
                 </TableHeader>
                 <TableBody selectable={false} displayRowCheckbox={false}>
-
                     {this.getEntryRows(this.props.entries, 0)}
-
                 </TableBody>
             </Table>
         );
     }
-/**
- * @description function used to return an array of rows with entries, necessary for sequence rows
- * @param entries entries to be shown in the table
- * @param depth used to generate unique keys since function can be called recursively
- */
+    /**
+     * @description function used to return an array of rows with entries, necessary for sequence rows
+     * @param entries entries to be shown in the table
+     * @param depth used to generate unique keys since function can be called recursively
+     */
     private getEntryRows(entries: DicomEntry[], depth: number): JSX.Element[] {
 
-        return entries.reduce((arr: JSX.Element[], entry, entryIndex) => {
+        return entries.reduce(
+            (arr: JSX.Element[], entry, entryIndex) => {
 
-            if (entry.sequence !== undefined) { 
-                // for each sequence entry modify tag to separate sequence tags from ordinary ones
-                for (var i = 0; i < entry.sequence.length; i++) {
-                    // prevent from adding multiple sequence delimiters
-                    if (entry.sequence[i].tagGroup.length === 4) {   
-                    entry.sequence[i].tagGroup = ' > ' + entry.sequence[i].tagGroup;
+                if (entry.sequence !== undefined) {
+                    // for each sequence entry modify tag to separate sequence tags from ordinary ones
+                    for (var i = 0; i < entry.sequence.length; i++) {
+                        // prevent from adding multiple sequence delimiters
+                        if (entry.sequence[i].tagGroup.length === 4) {
+                            entry.sequence[i].tagGroup = ' > ' + entry.sequence[i].tagGroup;
+                        }
+                    }
+
+                    arr.push(
+                        <DicomTableRow
+                            entry={entry}
+                            key={entryIndex + (10000 * (depth + 1))}
+                            shouldShowTag={true}
+                        />);
+                    arr = arr.concat(this.getEntryRows(entry.sequence, ++depth));
+                } else {
+
+                    // single row with single DicomEntry
+                    arr.push(
+                        <DicomTableRow
+                            entry={entry}
+                            key={entryIndex + (100000 * (depth + 1))}
+                            shouldShowTag={true}
+                        />
+                    );
                 }
-                }
-
-                arr.push(
-                <DicomTableRow 
-                    entry={entry} 
-                    key={entryIndex + (10000 * (depth + 1 ) )} 
-                    shouldShowTag={true} 
-                />);
-                arr = arr.concat(this.getEntryRows(entry.sequence, depth + 1));
-            } else {
-
-                // single row with single DicomEntry
-                arr.push(<DicomTableRow entry={entry} key={entryIndex + (10000 * (depth + 1))} shouldShowTag={true} />);
-            }
-            return arr;
-        },                    []);
+                return arr;
+            },
+            []);
     }
 }
--- src/client/components/dicom-table/dicom-table-row.tsx
+++ src/client/components/dicom-table/dicom-table-row.tsx
@@ -4,9 +4,11 @@ import { DicomEntry } from '../../model/dicom-entry';
 import { ColorDictionary } from '../../utils/colour-dictionary';
 import './dicom-table.css';
 
+var fileDownload = require('react-file-download');
+
 export interface DicomTableRowProps {
     entry: DicomEntry;
-    shouldShowTag: boolean; 
+    shouldShowTag: boolean;
 }
 
 export interface DicomTableRowState {
@@ -22,8 +24,8 @@ export class DicomTableRow extends React.Component<DicomTableRowProps, DicomTabl
     }
 
     public render() {
-        let tableRowStyle = { color: this.props.entry.colour};
-        let tagColor = { color: '#000000'};
+        let tableRowStyle = { color: this.props.entry.colour };
+        let tagColor = { color: '#000000' };
         let tableRowColumnStyle = {
             whiteSpace: 'normal',
             wordWrap: 'break-word'
@@ -34,16 +36,44 @@ export class DicomTableRow extends React.Component<DicomTableRowProps, DicomTabl
             tag = '';
             rowClass = '';
         }
+
+        let ele: JSX.Element;
+        if (this.props.entry.tagValue) {
+            if (this.props.entry.tagValue.toString().length > 100) {
+                let value = this.props.entry.tagValue.toString().substr(0, 100) + '...';
+                ele =
+                    (
+                        <span onClick={() => this.handleExcessiveText()}>
+                            <div className="plain-link">{value}</div>
+                            <div className="show-comment-on-hover">Click to downlad a text file with the tag value</div>
+                        </span>
+                    );
+            } else {
+                ele = <div>{this.props.entry.tagValue}</div>;
+            }
+        } else {
+            ele = <div />;
+        }
+
         return (
-                <TableRow style={tableRowStyle} className={rowClass}>
-                    <TableRowColumn style={tagColor}>
-                        {tag}
-                    </TableRowColumn>
-                    <TableRowColumn style={tableRowColumnStyle}>{this.props.entry.tagName}</TableRowColumn>
-                    <TableRowColumn style={tableRowColumnStyle}>{this.props.entry.tagValue}</TableRowColumn>
-                    <TableRowColumn>{this.props.entry.tagVR}</TableRowColumn>
-                    <TableRowColumn>{this.props.entry.tagVM}</TableRowColumn>
-                </TableRow>
+            <TableRow style={tableRowStyle} className={rowClass} >
+                <TableRowColumn style={tagColor}>
+                    {tag}
+                </TableRowColumn>
+                <TableRowColumn style={tableRowColumnStyle}>{this.props.entry.tagName}</TableRowColumn>
+                <TableRowColumn style={tableRowColumnStyle}>{ele}</TableRowColumn>
+                <TableRowColumn>{this.props.entry.tagVR}</TableRowColumn>
+                <TableRowColumn>{this.props.entry.tagVM}</TableRowColumn>
+            </TableRow>
         );
+    }
+
+    private handleExcessiveText() {
+        let val = this.props.entry.tagValue.toString();
+        if (val.length > 100) {
+            let blob = new Blob([val], { type: 'text/plain;charset=utf-8' });
+            let fileName = this.props.entry.tagName + '-value.txt';
+            fileDownload(blob, fileName);
+        }
     }
 }
