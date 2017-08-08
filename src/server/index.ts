0e6f76ba3905b0434fc52ff5f2b22e744fa3dd67
Miroslav Kubus <kubus.miro@gmail.com>
2017-08-08 17:30:21 +0200

    changed redirection of http to https for production

--- src/server/index.ts
+++ src/server/index.ts
@@ -4,10 +4,11 @@ const app = express();
 app.set('view engine', 'pug');
 app.use(express.static('public'));
 
-app.get('*', function (req, res) {
+app.get('/*', function (req, res) {
     if (req.hostname !== 'localhost') {
         res.redirect(process.env.root + req.url)
     }
+    console.log(process.env.root + req.url);
 })
 
 var port = process.env.PORT || 8080;
