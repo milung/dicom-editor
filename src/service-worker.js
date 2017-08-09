46fdd6a982f8d5c7fb825c3a8cf7f63b3a481ebd
Miroslav Kubus <kubus.miro@gmail.com>
2017-08-09 13:48:31 +0200

    try to fix problems with service worker part 2

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,14 +1,7 @@
 var CACHE_NAME = 'pwa-dicom-viewer-cache-v1';
 var urlsToCache = [
-    '/',
-    '/bundle.js',
-    '/index.html',
-    '/assets/js/cornerstone-library/cornerstoneWADOImageLoaderWebWorker.js',
-    '/assets/js/cornerstone-library/cornerstone.js',
-    '/assets/js/cornerstone-library/codecs/Allcodecs.min.js',
-    '/assets/manifest.json',
-    '/assets/img/logo.ico',
-    '/assets/img/logo.png'
+    '.',
+    'bundle.js'
 ];
 
 self.addEventListener('install', function (event) {
