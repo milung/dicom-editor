e81b5056596136bb660eb9c7075dd8b5b316892c
Miroslav Kubus <kubus.miro@gmail.com>
2017-08-09 10:15:35 +0200

    removed httpRedirect from server because of insecure error in pwa

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,11 +1,11 @@
 import * as express from 'express';
-import { redirectToHTTPS } from 'express-http-to-https';
+// import { redirectToHTTPS } from 'express-http-to-https';
 
 const app = express();
 
 app.set('view engine', 'pug');
 app.use(express.static('public'));
-app.use(redirectToHTTPS([/localhost:(\d{4})/], [/\/insecure/]));
+// app.use(redirectToHTTPS([/localhost:(\d{4})/], [/\/insecure/]));
 
 var port = process.env.PORT || 8080;
 app.listen(port);
