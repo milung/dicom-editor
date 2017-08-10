b7a912d7d38f61097ca63db131be549b036a1f68
Miroslav Kubus <kubus.miro@gmail.com>
2017-08-10 12:01:52 +0200

    webpack modifications for pwa working on production but not on localhost

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -47,6 +47,11 @@ module.exports = {
             template: './src/client/index.html'
         }),
         new webpack.HotModuleReplacementPlugin(),
+        new webpack.DefinePlugin({
+            'process.env': {
+                'NODE_ENV': JSON.stringify('dev')
+            }
+        }),
     ],
     devtool: 'inline-source-map',
     resolve: {
--- src/client/index.tsx
+++ src/client/index.tsx
@@ -7,6 +7,23 @@ import './index.css';
 
 injectTapEventPlugin();
 
+/**
+ * Serves for registration of service worker. Pwa is not working in developer mode
+ * but it is fully working in production mode.
+ */
+if (process.env.NODE_ENV === 'production') {
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('/service-worker.js')
+      .then(function (registration: ServiceWorkerRegistration) {
+        // tslint:disable-next-line:no-console
+        console.log('Registration succeeded. Scope is ' + registration.scope);
+      }).catch(function (err: Error) {
+        // tslint:disable-next-line:no-console
+        console.log('Registration failed with ' + err);
+      });
+  }
+}
+
 ReactDOM.render(
   <AppContainer />,
   document.getElementById('root')
