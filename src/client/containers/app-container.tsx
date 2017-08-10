551e9f6f91c0ee0b8ee9818b14a09af573ecb426
Lubomira Trnavska <lubomira.trnavska@gmail.com>
2017-08-10 13:19:26 +0200

    color touch up

--- src/client/containers/app-container.tsx
+++ src/client/containers/app-container.tsx
@@ -1,14 +1,31 @@
 import * as React from 'react';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import { BrowserRouter, Route } from 'react-router-dom';
 import App from '../app';
-// import UI from '../ui';
+
+const muiTheme = getMuiTheme({
+  palette: {
+    primary1Color: '#009999',
+    borderColor: '#009e9e'
+  },
+
+  tabs: {
+    backgroundColor: '#daeded',
+    textColor: '#009999',
+    selectedTextColor: '#016d6d',
+  },
+
+  inkBar: {
+    backgroundColor: '#009999'
+  }
+});
 
 export default class AppContainer extends React.Component<{}, {}> {
   render() {
     return (
       <div>
-        <MuiThemeProvider>
+        <MuiThemeProvider muiTheme={muiTheme}>
           <BrowserRouter>
             {/*<Route path="/" component={UI} />*/}
             <Route path="/" component={App} />
