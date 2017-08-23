74122f031a5496d86d5a9ebc2f7934c8e0602d3d
Miroslav Kubus <kubus.miro@gmail.com>
2017-08-23 15:36:10 +0200

    try to set up gzip compression

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,17 +1,20 @@
 import * as express from 'express';
 
 const app = express();
+var compression = require('compression')
 
 app.set('view engine', 'pug');
 app.use(express.static('public'));
+app.use(compression());
 
-app.get('*.js', function (req, res, next) {
-  console.log('Serving .gz instead of .js for better performance');
-  req.url = req.url + '.gz';
-  res.set('Content-Encoding', 'gzip');
-  res.set('Content-Type', 'text/javascript');
-  next();
-});
+// Dynamic loading of gz insted of js to increase performance
+// app.get('*.js', function (req, res, next) {
+//   console.log('Serving .gz instead of .js for better performance');
+//   req.url = req.url + '.gz';
+//   res.set('Content-Encoding', 'gzip');
+//   res.set('Content-Type', 'text/javascript');
+//   next();
+// });
 
 var port = process.env.PORT || 8080;
 app.listen(port);
