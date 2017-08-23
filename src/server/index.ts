1f133c06837ad67b790efadd770709dedef2c8ee
Miroslav Kubus <kubus.miro@gmail.com>
2017-08-23 15:24:47 +0200

    compression fix

--- src/server/index.ts
+++ src/server/index.ts
@@ -6,8 +6,10 @@ app.set('view engine', 'pug');
 app.use(express.static('public'));
 
 app.get('*.js', function (req, res, next) {
+  console.log('Serving .gz instead of .js for better performance');
   req.url = req.url + '.gz';
   res.set('Content-Encoding', 'gzip');
+  res.set('Content-Type', 'text/javascript');
   next();
 });
 
