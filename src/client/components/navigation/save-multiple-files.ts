a24bdaeadfa2ef03f53614b3f86cd686986db5a1
Filip Mrocek <mrocek.filip@gmail.com>
2017-08-31 16:03:06 +0200

    776 - chagnes are applied after pop up dialog for overriding is shown

--- src/client/components/navigation/save-multiple-files.ts
+++ src/client/components/navigation/save-multiple-files.ts
@@ -21,12 +21,43 @@ export class MultiSave {
      * @param {HeavyweightFile} file file to save
      */
     public saveFile(file: HeavyweightFile) {
-        let lightFile = convertHeavyToLight(file);
-        let dbKey = saveFileIntoSavedDb(file);
-        lightFile.dbKey = dbKey;
-        let recentFileUtil: RecentFileStoreUtil = new RecentFileStoreUtil(this.reducer);
-        recentFileUtil.handleStoringRecentFile(lightFile);
-        this.reducer.addSavedFile(lightFile);
+
+        // apply changes to file buffer
+        let dicomEditor: DicomEditor = new DicomEditor();
+        let updatedBuffer = dicomEditor.applyAllChanges(file);
+
+        let dicomReader = new DicomReader();
+        let loadedFromReducer = this.reducer.findLoadedFileByName(file.fileName);
+
+        // create copy of loaded file with parsed dicom entries from updated buffer
+        if (loadedFromReducer) {
+            let loadedCopy: HeavyweightFile = {
+                bufferedData: updatedBuffer,
+                dicomData: dicomReader.getDicomEntries(updatedBuffer), // get newly parsed dicom data
+                fileSize: loadedFromReducer.fileSize,
+                fileName: loadedFromReducer.fileName,
+                timestamp: loadedFromReducer.timestamp,
+                unsavedChanges: undefined
+            };
+            // remove unsaved changes from file
+            loadedFromReducer.unsavedChanges = [];
+
+            // remove loaded file so it can be updated
+            this.reducer.removeLoadedFiles([loadedFromReducer]);
+
+            // update loaded file's copy that has updated buffer
+            this.reducer.addLoadedFiles([loadedCopy]);
+            storeFilesToDB(this.reducer);
+
+            let lightFile = convertHeavyToLight(loadedCopy);
+            let dbKey = saveFileIntoSavedDb(loadedCopy);
+            lightFile.dbKey = dbKey;
+            let recentFileUtil: RecentFileStoreUtil = new RecentFileStoreUtil(this.reducer);
+            recentFileUtil.handleStoringRecentFile(lightFile);
+            this.reducer.addSavedFile(lightFile);
+        }
+
+
     }
     public handleSaveClick(disabled: boolean) {
         if (!disabled) {
@@ -46,35 +77,10 @@ export class MultiSave {
         let inConflict: HeavyweightFile[] = [];
         for (var i = 0; i < toBeSaved.length; i++) {
 
-            // apply changes to file buffer
-            let dicomEditor: DicomEditor = new DicomEditor();
-            let updatedBuffer = dicomEditor.applyAllChanges(toBeSaved[i]);
-
-            let dicomReader = new DicomReader();
-            let loadedReducer = this.reducer.findLoadedFileByName(toBeSaved[i].fileName);
-
-            // create copy of loaded file with parsed dicom entries from updated buffer
-            if (loadedReducer) {
-                let loadedCopy: HeavyweightFile = {
-                    bufferedData: updatedBuffer,
-                    dicomData: dicomReader.getDicomEntries(updatedBuffer), // get newly parsed dicom data
-                    fileSize: loadedReducer.fileSize,
-                    fileName: loadedReducer.fileName,
-                    timestamp: loadedReducer.timestamp,
-                    unsavedChanges: undefined
-                };
-                // remove loaded file so it can be updated
-                this.reducer.removeLoadedFiles([loadedReducer]);
-
-                // update loaded file's copy that has updated buffer
-                this.reducer.addLoadedFiles([loadedCopy]);
-                storeFilesToDB(this.reducer);
-
-                // save updated file
-                let success = await this.tryToSaveOneFile(loadedCopy);
-                if (!success) {
-                    inConflict.push(loadedCopy);
-                }
+            // save updated file
+            let success = await this.tryToSaveOneFile(toBeSaved[i]);
+            if (!success) {
+                inConflict.push(toBeSaved[i]);
             }
 
         }
